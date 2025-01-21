81b341b72730b5a34dfd63b7f0c488b2db983041
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2025-01-21 11:57:41 +0330

    add some stuff related to colorscheme

--- lua/reconfig-vim/plugins/indent-blankline.lua
+++ lua/reconfig-vim/plugins/indent-blankline.lua
@@ -1,8 +1,15 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" },
-  },
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	opts = function(_, opts)
+		-- Add the dotted line configuration
+		opts.indent = { char = "┊" }
+
+		-- Other blankline configuration here
+		return require("indent-rainbowline").make_opts(opts)
+	end,
+	dependencies = {
+		"TheGLander/indent-rainbowline.nvim",
+	},
 }
